c6caade438565681541472c210ad02d8657f3bf1
Yonaba <roland.yonaba@gmail.com>
2014-08-30 12:49:46 +0000

    Tabs to 2spaces

--- src/menu.lua
+++ src/menu.lua
@@ -15,37 +15,37 @@ font:setPixelSizes(25,25)
 oldpad =Controls.read()
 
 while true do
-	screen:clear()
-	pad = Controls.read()
+  screen:clear()
+  pad = Controls.read()
 
-	screen:blit(0,0,wall)
+  screen:blit(0,0,wall)
 
-	if pad:down() and not oldpad:down() then selector = selector +1
-	elseif pad:up() and not oldpad:up() then selector = selector -1
-	end
-	 
-	if selector < 1 then selector = 2 
-	elseif selector > 2 then selector = 1 
-	end 
+  if pad:down() and not oldpad:down() then selector = selector +1
+  elseif pad:up() and not oldpad:up() then selector = selector -1
+  end
+   
+  if selector < 1 then selector = 2 
+  elseif selector > 2 then selector = 1 
+  end 
 
-	if selector == 1 then screen:fillRect(50,79,65,20,shadow) end
-	if selector == 2 then screen:fillRect(50,99,73,20,shadow) end
+  if selector == 1 then screen:fillRect(50,79,65,20,shadow) end
+  if selector == 2 then screen:fillRect(50,99,73,20,shadow) end
 
-	screen:fontPrint(font,55,100,"Jouer",blue)
-	screen:fontPrint(font,55,120,"Sortir",blue)
+  screen:fontPrint(font,55,100,"Jouer",blue)
+  screen:fontPrint(font,55,120,"Sortir",blue)
 
-	if pad:cross() and not oldpad:cross() and selector == 1 then
-		dofile("game.lua")
-		file:close()
-	end
+  if pad:cross() and not oldpad:cross() and selector == 1 then
+    dofile("game.lua")
+    file:close()
+  end
 
-	if pad:cross() and not oldpad:cross() and selector == 2 then
-		System.Quit()
-	end
+  if pad:cross() and not oldpad:cross() and selector == 2 then
+    System.Quit()
+  end
 
-	oldpad=pad
-	screen.waitVblankStart()
-	screen.flip()
-	pad=nil
+  oldpad=pad
+  screen.waitVblankStart()
+  screen.flip()
+  pad=nil
 end
 
