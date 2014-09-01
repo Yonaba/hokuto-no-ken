7538f40759fa50d2b7f9d4eeff2f51a8cb9110b0
Yonaba <roland.yonaba@gmail.com>
2014-09-01 12:50:37 +0000

    Tabs to 2spaces

--- src/script.lua
+++ src/script.lua
@@ -5,42 +5,42 @@ faderColor = Color.new(0,0,0,alphaValue)
 fader:clear(faderColor)
 
 while true do
-	screen:clear()
-	screen:blit(0,0,BackGround_Cache)
-	screen:blit(0,0,fader)
-	if alphaValue > 0 then
-		alphaValue = alphaValue - 10
-		else
-		break
-	end
-	faderColor = Color.new(0,0,0,alphaValue)
-	fader:clear(faderColor)
-	
-	screen.waitVblankStart()
-	screen.flip()
+  screen:clear()
+  screen:blit(0,0,BackGround_Cache)
+  screen:blit(0,0,fader)
+  if alphaValue > 0 then
+    alphaValue = alphaValue - 10
+    else
+    break
+  end
+  faderColor = Color.new(0,0,0,alphaValue)
+  fader:clear(faderColor)
+  
+  screen.waitVblankStart()
+  screen.flip()
 end
 
 screen.waitVblankStart(50) 
 
 while true do
-	screen:clear()
-	screen:blit(0,0,BackGround_Cache)
-	screen:blit(0,0,fader)
-	if alphaValue < 255 then
-		alphaValue = alphaValue + 10
-		else
-		break
-	end
-	faderColor = Color.new(0,0,0,alphaValue)
-	fader:clear(faderColor)
-	
-	screen.waitVblankStart()
-	screen.flip()
+  screen:clear()
+  screen:blit(0,0,BackGround_Cache)
+  screen:blit(0,0,fader)
+  if alphaValue < 255 then
+    alphaValue = alphaValue + 10
+    else
+    break
+  end
+  faderColor = Color.new(0,0,0,alphaValue)
+  fader:clear(faderColor)
+  
+  screen.waitVblankStart()
+  screen.flip()
 end
 
 
 while true do
-	dofile("menu.lua")
+  dofile("menu.lua")
 end
 
 
